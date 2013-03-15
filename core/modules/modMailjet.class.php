d72cbfeecbf23d5053b179c0ee173b229bebe03f
fhenry <florian.henry@open-concept.pro>
2013-03-15 15:23:13 +0100

    Increment version

--- core/modules/modMailjet.class.php
+++ core/modules/modMailjet.class.php
@@ -52,7 +52,7 @@ class modMailjet extends DolibarrModules
 
 		$this->description = "Mailjet Connector";
 
-		$this->version = '1.1';
+		$this->version = '1.2';
 
 		$this->const_name = 'MAIN_MODULE_' . strtoupper($this->name);
 
