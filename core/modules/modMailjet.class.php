c8e3f2c0a85414959a3355578a0ef75075192e1f
jfefe <jfefe@aternatik.fr>
2013-05-23 13:16:13 +0200

    Start version 1.8

--- core/modules/modMailjet.class.php
+++ core/modules/modMailjet.class.php
@@ -52,7 +52,7 @@ class modMailjet extends DolibarrModules
 
 		$this->description = "Mailjet Connector";
 
-		$this->version = '1.7';
+		$this->version = '1.8';
 
 		$this->const_name = 'MAIN_MODULE_' . strtoupper($this->name);
 
